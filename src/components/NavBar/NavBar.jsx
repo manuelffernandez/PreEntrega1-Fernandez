843a2714f04da7a516d6782db13d2c8687be4e49
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-11-19 14:05:31 -0300

    componentizes cart icon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,6 @@
 import NavItem from '../NavItem/NavItem';
+import CartWidget from '../CartWidget/CartWidget';
+
 import './NavBar.css';
 
 const NavBar = () => {
@@ -12,9 +14,7 @@ const NavBar = () => {
                   </a>
                </div>
                <div className='order-2 col-2 offset-5 order-sm-3 offset-sm-0 me-sm-1 d-flex justify-content-end'>
-                  <button className='p-0 border-0 bg-transparent'>
-                     <i className='fa-sharp fa-solid fa-cart-shopping fs-3 text-light cart'></i>
-                  </button>
+                  <CartWidget />
                </div>
                <button
                   id='caca'
