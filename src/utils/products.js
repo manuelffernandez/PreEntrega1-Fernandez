cf6122d3204d8b8243f95f51eb906a1d245f1acb
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-04 15:01:00 -0300

    creates products array

---
+++ src/utils/products.js
@@ -0,0 +1,132 @@
+const categories = {
+  accessory: 'Accesorios',
+  clothes: 'Indumentaria',
+  shoes: 'Zapatos',
+};
+
+export const products = [
+  {
+    id: 1,
+    name: 'Sombrero de paja',
+    stock: 30,
+    price: 50,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949299/cap-gda9812fe2_1920_xjbdrs.jpg',
+    categoryName: categories.accessory,
+  },
+  {
+    id: 2,
+    name: 'Camisa rallada',
+    stock: 20,
+    price: 70,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949295/shirts-g920f30f7f_1920_qyvqls.jpg',
+    categoryName: categories.clothes,
+  },
+  {
+    id: 3,
+    name: 'Zapatos de cuero',
+    stock: 10,
+    price: 100,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949295/wingtip-gb4e423386_1920_gvee9l.jpg',
+    categoryName: categories.shoes,
+  },
+  {
+    id: 4,
+    name: 'Cinturón de cuero',
+    stock: 15,
+    price: 40,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949295/menswear-g618a62665_1920_qgn79t.jpg',
+    categoryName: categories.accessory,
+  },
+  {
+    id: 5,
+    name: 'Moño',
+    stock: 5,
+
+    price: 30,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949294/tie-g00d1aefd3_1920_hnhjrm.jpg',
+    categoryName: categories.accessory,
+  },
+  {
+    id: 6,
+    name: 'Guantes de lana',
+    stock: 20,
+    price: 35,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949294/gloves-g432262680_1920_lhc92o.jpg',
+    categoryName: categories.clothes,
+  },
+  {
+    id: 7,
+    name: 'Billetera de cuero',
+    stock: 10,
+    price: 45,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949294/skin-g433a984d2_1920_jr9voh.jpg',
+    categoryName: categories.accessory,
+  },
+  {
+    id: 8,
+    name: 'Lentes de sol',
+    stock: 5,
+
+    price: 60,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949293/accessory-g0e35c37d8_1920_vec1ck.jpg',
+    categoryName: categories.accessory,
+  },
+  {
+    id: 9,
+    name: 'Reloj de muñeca',
+    stock: 10,
+    price: 70,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949293/accessory-g984ee5096_1920_ctjsd7.jpg',
+    categoryName: categories.accessory,
+  },
+  {
+    id: 10,
+    name: 'Botas de cuero',
+    stock: 7,
+
+    price: 120,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669953528/boots-g53e2dc944_1920_ciakjx.jpg',
+    categoryName: categories.shoes,
+  },
+  {
+    id: 11,
+    name: 'Swater de lana',
+    stock: 10,
+    price: 80,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
+    image:
+      'https://res.cloudinary.com/dke7ynizq/image/upload/v1669954012/fashion-g85e5c5ef8_1920_hglylt.jpg',
+    categoryName: categories.clothes,
+  },
+];
