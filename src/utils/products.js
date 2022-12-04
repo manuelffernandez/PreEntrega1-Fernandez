97817ec7714463e5e27ca0b375d587f41c49151d
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-04 17:34:35 -0300

    adds categories array in separated file and exports it

--- src/utils/products.js
+++ src/utils/products.js
@@ -1,8 +1,4 @@
-const categories = {
-  accessory: 'Accesorios',
-  clothes: 'Indumentaria',
-  shoes: 'Zapatos',
-};
+import { categories } from './categories';
 
 export const products = [
   {
@@ -14,7 +10,7 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949299/cap-gda9812fe2_1920_xjbdrs.jpg',
-    categoryName: categories.accessory,
+    categoryName: categories[0].category,
   },
   {
     id: 2,
@@ -25,7 +21,7 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949295/shirts-g920f30f7f_1920_qyvqls.jpg',
-    categoryName: categories.clothes,
+    categoryName: categories[1].category,
   },
   {
     id: 3,
@@ -36,7 +32,7 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949295/wingtip-gb4e423386_1920_gvee9l.jpg',
-    categoryName: categories.shoes,
+    categoryName: categories[2].category,
   },
   {
     id: 4,
@@ -47,7 +43,7 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949295/menswear-g618a62665_1920_qgn79t.jpg',
-    categoryName: categories.accessory,
+    categoryName: categories[0].category,
   },
   {
     id: 5,
@@ -59,7 +55,7 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949294/tie-g00d1aefd3_1920_hnhjrm.jpg',
-    categoryName: categories.accessory,
+    categoryName: categories[0].category,
   },
   {
     id: 6,
@@ -70,7 +66,7 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949294/gloves-g432262680_1920_lhc92o.jpg',
-    categoryName: categories.clothes,
+    categoryName: categories[1].category,
   },
   {
     id: 7,
@@ -81,7 +77,7 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949294/skin-g433a984d2_1920_jr9voh.jpg',
-    categoryName: categories.accessory,
+    categoryName: categories[0].category,
   },
   {
     id: 8,
@@ -93,7 +89,7 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949293/accessory-g0e35c37d8_1920_vec1ck.jpg',
-    categoryName: categories.accessory,
+    categoryName: categories[0].category,
   },
   {
     id: 9,
@@ -104,7 +100,7 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669949293/accessory-g984ee5096_1920_ctjsd7.jpg',
-    categoryName: categories.accessory,
+    categoryName: categories[0].category,
   },
   {
     id: 10,
@@ -116,7 +112,7 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669953528/boots-g53e2dc944_1920_ciakjx.jpg',
-    categoryName: categories.shoes,
+    categoryName: categories[2].category,
   },
   {
     id: 11,
@@ -127,6 +123,6 @@ export const products = [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer dolor libero, tincidunt id lacinia auctor, mattis elementum tortor. In eu bibendum magna, ac dignissim justo.',
     image:
       'https://res.cloudinary.com/dke7ynizq/image/upload/v1669954012/fashion-g85e5c5ef8_1920_hglylt.jpg',
-    categoryName: categories.clothes,
+    categoryName: categories[1].category,
   },
 ];
