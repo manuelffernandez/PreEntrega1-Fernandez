71d61c95857de9171f7fa70081ed910cf1354846
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-04 18:09:36 -0300

    creates ItemList component

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,26 @@
+import Item from '../Item/Item';
+
+const ItemList = ({ products }) => {
+  return (
+    <div className='row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-3'>
+      {products.length > 0 ? (
+        products.map(product => {
+          return (
+            <Item
+              key={product.id}
+              pictUrl={product.image}
+              name={product.name}
+              stock={product.stock}
+              price={product.price}
+              categoryName={product.categoryName}
+            />
+          );
+        })
+      ) : (
+        <p>Cargando...</p>
+      )}
+    </div>
+  );
+};
+
+export default ItemList;
