0a94718b30eafdd5d853bfa05e29925b27c8c2bd
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-05 01:47:17 -0300

    adds Routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import DescriptionContainer from './components/DescriptionContainer/DescriptionContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
-      <ItemListContainer />
-      <DescriptionContainer />
-    </>
+      <Routes>
+        <Route path='/' element={<ItemListContainer />} />
+        <Route path='/category/:categoryName' element={<ItemListContainer />} />
+        <Route path='/item/:itemID' element={<DescriptionContainer />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
