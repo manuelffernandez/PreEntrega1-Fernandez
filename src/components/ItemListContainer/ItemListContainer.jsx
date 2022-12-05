ddd18d73028049c500857564a9492def5f12ebb1
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-05 01:51:34 -0300

    adds useParams into ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,12 +2,22 @@ import { useEffect, useState } from 'react';
 import { products } from '../../utils/products';
 import ItemList from '../ItemList/ItemList';
 import { customFetch } from '../../utils/customFetch';
+import { useParams } from 'react-router-dom';
 
 const ItemListContainer = () => {
   const [data, setData] = useState([]);
+  const { categoryName } = useParams();
 
   useEffect(() => {
-    customFetch(2000, products)
+    let productsToShow = [];
+    if (categoryName) {
+      productsToShow = products.filter(
+        product => product.categoryName === categoryName
+      );
+    } else {
+      productsToShow = products;
+    }
+    customFetch(2000, productsToShow)
       .then(prods => setData(prods))
       .catch(err => console.log(err));
   }, [data]);
