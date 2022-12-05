bcff2d9f4834b538949b02a808c1d36af8cd468c
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-05 01:52:29 -0300

    adds useParams into DescriptionContainer

--- src/components/DescriptionContainer/DescriptionContainer.jsx
+++ src/components/DescriptionContainer/DescriptionContainer.jsx
@@ -3,14 +3,19 @@ import './DescriptionContainer.css';
 import { products } from '../../utils/products';
 import Description from '../Description/Description';
 import { customFetch } from '../../utils/customFetch';
+import { useParams } from 'react-router-dom';
 
 const DescriptionContainer = () => {
   const [product, setProduct] = useState({});
+  const { itemID } = useParams();
 
   useEffect(() => {
-    customFetch(2000, products[2])
-      .then(prods => {
-        setProduct(prods);
+    customFetch(
+      2000,
+      products.find(product => product.id === itemID)
+    )
+      .then(prod => {
+        setProduct(prod);
       })
       .catch(err => console.log(err));
   }, []);
