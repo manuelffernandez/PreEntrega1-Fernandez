9b418e990e2b60f7367b0afedaefe2abc9e85565
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-05 01:46:59 -0300

    refact cartWidget comp

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,13 @@
 import './CartWidget.css';
 
 const CartWidget = () => {
-   return (
+  return (
+    <div className='order-2 col-2 offset-5 order-sm-3 offset-sm-0 me-sm-1 d-flex justify-content-end'>
       <button className='p-0 border-0 bg-transparent'>
-         <i className='fa-sharp fa-solid fa-cart-shopping fs-3 text-light cart'></i>
+        <i className='fa-sharp fa-solid fa-cart-shopping fs-3 text-light cart'></i>
       </button>
-   );
+    </div>
+  );
 };
 
 export default CartWidget;
