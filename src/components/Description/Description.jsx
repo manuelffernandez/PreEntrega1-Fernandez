c82845f9afa4adf47dce6ceaf299f35970c110e3
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-07 02:41:52 -0300

    refacts layout of Description comp&adds 'onAdd' function to it

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,7 +1,12 @@
+import ItemCount from '../ItemCount/ItemCount';
 import './Description.css';
 
 const Description = props => {
   const { product } = props;
+
+  const onAdd = (amount, productName) => {
+    alert(`Agregaste ${amount} ${productName} a tu carrito`);
+  };
 
   return (
     <div className='container-fluid row row-cols-1 row-cols-lg-2 mx-auto'>
@@ -27,13 +32,20 @@ const Description = props => {
           </p>
         </div>
         <hr />
-        <div className='row row-cols-2 '>
-          <div className='col text-start'>
-            <p className='m-0 fs-5 roboto_600 colorBlueGreen'>
+        <div className='row'>
+          <div className='col-12 col-sm-3 d-flex align-items-center'>
+            <p className='mx-auto m-0 fs-5 roboto_600 colorBlueGreen'>
               Stock:<span className='roboto_300'>{product.stock}</span>
             </p>
           </div>
-          <div className='col'>boton</div>
+          <div className='col-12 col-sm-9'>
+            <ItemCount
+              stock={product.stock}
+              name={product.name}
+              initialAmount={1}
+              onAdd={onAdd}
+            />
+          </div>
         </div>
       </div>
     </div>
