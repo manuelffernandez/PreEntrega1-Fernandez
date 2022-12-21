d445359abfcf8f55fefa18ce9c309b0fa3b5e564
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-20 23:07:52 -0300

    creates route to CartList & provides cart context to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import DescriptionContainer from './components/DescriptionContainer/DescriptionContainer';
+import Cart from './components/Cart/Cart';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CartContextProvider from './components/CartContext/CartContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<ItemListContainer />} />
-        <Route
-          path='/categoria/:categoryName'
-          element={<ItemListContainer />}
-        />
-        <Route path='/item/:itemID' element={<DescriptionContainer />} />
-      </Routes>
-    </BrowserRouter>
+    <CartContextProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<ItemListContainer />} />
+          <Route
+            path='/categoria/:categoryName'
+            element={<ItemListContainer />}
+          />
+          <Route path='/item/:itemID' element={<DescriptionContainer />} />
+          <Route path='/cart' element={<Cart />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </CartContextProvider>
   );
 }
 
