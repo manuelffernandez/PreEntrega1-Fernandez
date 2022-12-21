a45a1723e6c464b90c61f50a5bb61342c3f025d0
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-21 03:03:08 -0300

    creates calc functions in CartContext file

--- src/components/CartContext/CartContext.jsx
+++ src/components/CartContext/CartContext.jsx
@@ -37,8 +37,51 @@ const CartContextProvider = props => {
     setCartList([]);
   };
 
+  const calcTotalItemAmount = () => {
+    let qty = 0;
+    cartList.forEach(item => {
+      qty += item.amount;
+    });
+    return qty;
+  };
+
+  const calcItemSubtotal = itemId => {
+    const found = cartList.find(item => item.id === itemId);
+    const subtotal = found.price * found.amount;
+    return subtotal;
+  };
+
+  const calcSubtotal = () => {
+    let total = 0;
+
+    cartList.forEach(item => {
+      total += calcItemSubtotal(item.id);
+    });
+    return total;
+  };
+
+  const calcTaxes = () => {
+    const taxRate = 0.21;
+    return Math.floor(calcSubtotal() * taxRate);
+  };
+
+  const calcTotal = () => {
+    return calcSubtotal() + calcTaxes();
+  };
+
   return (
-    <CartContext.Provider value={{ cartList, addItem, removeItem, clear }}>
+    <CartContext.Provider
+      value={{
+        cartList,
+        addItem,
+        removeItem,
+        clear,
+        calcTotalItemAmount,
+        calcItemSubtotal,
+        calcSubtotal,
+        calcTaxes,
+        calcTotal,
+      }}>
       {children}
     </CartContext.Provider>
   );
