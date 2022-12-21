1db4857f2a73d934840f574576510d382fd4575b
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-20 23:07:19 -0300

    add link to cartWidget component

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const CartWidget = () => {
   return (
     <div className='order-2 col-1 offset-5 order-sm-3 offset-sm-0 me-sm-1 d-flex justify-content-end'>
-      <button className='p-0 border-0 bg-transparent'>
-        <i className='fa-sharp fa-solid fa-cart-shopping fs-3 colorYellow hoverBlueGreen'></i>
-      </button>
+      <Link to='/cart' style={{ textDecoration: 'none' }}>
+        <button className='p-0 border-0 bg-transparent'>
+          <i className='fa-sharp fa-solid fa-cart-shopping fs-3 colorYellow hoverBlueGreen'></i>
+        </button>
+      </Link>
     </div>
   );
 };
