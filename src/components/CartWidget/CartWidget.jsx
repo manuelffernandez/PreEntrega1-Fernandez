fdbe531a8f64eceedcedaefcf3193dbca3bdfde0
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-26 13:23:27 -0300

    conditional render for CartWidget amount

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -4,14 +4,17 @@ import { useContext } from 'react';
 
 const CartWidget = () => {
   const { calcTotalItemAmount } = useContext(CartContext);
+  const totalItemAmount = calcTotalItemAmount();
   return (
     <div className='order-2 col-1 offset-5 order-sm-3 offset-sm-0 me-sm-1 d-flex justify-content-end'>
       <Link to='/cart' style={{ textDecoration: 'none' }}>
         <button className='position-relative p-0 border-0 bg-transparent'>
           <i className='fa-sharp fa-solid fa-cart-shopping fs-3 colorYellow hoverBlueGreen'></i>
-          <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>
-            {calcTotalItemAmount()}
-          </span>
+          {totalItemAmount ? (
+            <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>
+              {calcTotalItemAmount()}
+            </span>
+          ) : null}
         </button>
       </Link>
     </div>
