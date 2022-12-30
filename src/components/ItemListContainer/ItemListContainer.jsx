9180851e7826f69842b3a8a8afb717da144597b4
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-30 02:52:16 -0300

    refacts useEffect on ItemListContainer comp

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,25 +1,18 @@
 import { useEffect, useState } from 'react';
-import { products } from '../../utils/products';
 import ItemList from '../ItemList/ItemList';
-import { customFetch } from '../../utils/customFetch';
 import { useParams } from 'react-router-dom';
+import { getProducts } from '../../services/fetchData';
 
 const ItemListContainer = () => {
   const [data, setData] = useState([]);
   const { categoryName } = useParams();
 
   useEffect(() => {
-    let productsToShow = [];
-    if (categoryName) {
-      productsToShow = products.filter(
-        product => product.categoryName === categoryName
-      );
-    } else {
-      productsToShow = products;
-    }
-    customFetch(1000, productsToShow)
-      .then(prods => setData(prods))
-      .catch(err => console.log(err));
+    getProducts(categoryName)
+      .then(products => setData(products))
+      .catch(err => {
+        console.error('There was an error!', err);
+      });
   }, [categoryName]);
 
   return (
