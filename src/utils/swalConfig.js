0320882e704b88b1cdec5056bed922ddacc35d87
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-12-30 23:50:37 -0300

    creates swal functions and swal config objs

---
+++ src/utils/swalConfig.js
@@ -0,0 +1,29 @@
+const loader = {
+  title: 'Cargando',
+  html: `<div class='loadingio-spinner-eclipse-2jj7ryb2t5c'>
+          <div class='ldio-b9u4anwey1'>
+            <div></div>
+          </div>
+        </div>`,
+};
+
+const addedItem = {
+  title: 'Producto agregado',
+  confirmButtonText: 'Aceptar',
+};
+
+const paySuccess = {
+  title: 'Compra realizada',
+  icon: 'success',
+  confirmButtonText: 'Aceptar',
+};
+
+const error = {
+  title: 'Ocurrió un error',
+  icon: 'error',
+  confirmButtonText: 'Aceptar',
+};
+
+const swalConfig = { paySuccess, addedItem, loader, error };
+
+export default swalConfig;
